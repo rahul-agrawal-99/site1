6c29478544d00f7adbc3d553c1e59bc1e0df34af
Rahul Agrawal <77106010+rahul-agrawal-99@users.noreply.github.com>
2022-09-02 16:39:29 +0530

    Update file.sh

--- script/file.sh
+++ script/file.sh
@@ -1,5 +1,12 @@
 cd /home/ubuntu/site1/www
 npm install
 npm run build
+if [ -e /etc/nginx/sites-enabled/site1.conf ]
+then
+    echo "File already Exists so rm link then again estabilishing link"
+    sudo rm /etc/nginx/sites-enabled/site1.conf
+else
+    echo "File not exists so good for US"
+fi
 sudo ln -s /home/ubuntu/site1/site1.conf /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
