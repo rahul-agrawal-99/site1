7102acf588646b6d56f73ebc13a673a98df82488
Rahul Agrawal <77106010+rahul-agrawal-99@users.noreply.github.com>
2022-09-02 15:48:36 +0530

    Update file.sh

--- script/file.sh
+++ script/file.sh
@@ -1,7 +1,7 @@
 cd ~/site1/
 git pull 
 cd ~/site1/www/
-npm install
-npm run build
+sudo npm install
+sudo npm run build
 sudo ln -s ~/site1/site1.conf /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
